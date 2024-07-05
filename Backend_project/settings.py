cf72b6cc5b41063c163b9e535ee6a60356e8e84f
Luis <luisernestoperezbello@gmail.com>
2024-07-05 01:03:03 -0600

    version 3 to deploy

--- Backend_project/settings.py
+++ Backend_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&)8&q!o!f9)qt8m2%68p(o^tzrrrly-^f^z(pf%wi+p@cd=p=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
